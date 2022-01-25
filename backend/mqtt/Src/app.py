659921df9735790f27addb41c43877983e768303
Sam Verdoodt <sam.verdoodt@student.howest.be>
2022-01-25 14:05:35 +0100

    Added return

--- backend/mqtt/Src/app.py
+++ backend/mqtt/Src/app.py
@@ -5,9 +5,10 @@ import paho.mqtt.publish as publish
 import json
 import time
 import ssl
-from Src import pytemi as temi
+import pytemi as temi
 
 LOCATIE = None
+GUID = None
 
 HOST = "40.113.96.140"
 PORT = 1883
@@ -15,13 +16,14 @@ TEMI_SERIAL = "00121175512"
 ROBOT = ""
 # CA_CRT = "C:/ca.crt"
 
+
 def on_connect(client, userdata, flags, rc):
     global LOCATIE
 
     print("[STATUS] Connected to: {} (rc:{})".format(client._client_id, rc))
     client.subscribe("F2B/connection")
     client.subscribe("F2B/locatie")
-
+    client.subscribe("F2B/return")
     # subscribe to Temi To Back (Jave Mqtt Client) & Front To Back (JavaScript Mqtt Client) topics
     # client.subscribe("Temi/...")
 
@@ -30,41 +32,56 @@ def on_disconnect(client, userdata, rc):
     print(f"Disconnected with result code {rc}")
     client.loop_stop()
 
+
 def on_message(client, userdata, msg):
     global LOCATIE
-
+    global GUID
     print(msg.topic + " " + str(msg.payload))
     print(f"Raw message: {msg.payload}")
     print("------------------------------------------------------")
 
-    data = msg.payload.decode("utf-8")
-    dict = json.loads(data)
+    topic = msg.topic
+    if topic == "F2B/locatie":
+        data = msg.payload.decode("utf-8")
+        dict = json.loads(data)
 
-    if "connectionStatus" in dict.keys():
-        LOCATIE = None
-        client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-        ROBOT.webview("https://temi-tablet.azurewebsites.net/")
+        if "connectionStatus" in dict.keys():
+            LOCATIE = None
+            client.publish(
+                "B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            ROBOT.webview("https://temi-tablet.azurewebsites.net/")
 
-    if "locatie" in dict.keys():
+        if "locatie" in dict.keys():
+            LOCATIE = dict["locatie"]
+            client.publish(
+                "B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            # for testing
+
+        if "status" in dict.keys():
+            if(LOCATIE == "onderweg naar kleedkamer" and dict["status"] == "gearriveerd"):
+                LOCATIE = "kleedkamer"
+                client.publish(
+                    "B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            elif(LOCATIE == "onderweg naar sportscube" and dict["status"] == "gearriveerd"):
+                LOCATIE = "sportscube"
+                client.publish(
+                    "B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            elif(LOCATIE == "onderweg naar onthaal" and dict["status"] == "gearriveerd"):
+                LOCATIE = "onthaal"
+                client.publish(
+                    "B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            else:
+                print(f"ERROR PARSING 'ARRIVED', CURRENT LOCATION: {LOCATIE}")
+
+    elif topic == "F2B/return":
+        data = msg.payload.decode("utf-8")
+        dict = json.loads(data)
         LOCATIE = dict["locatie"]
-        client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-        # for testing
+        GUID = dict["GUID"]
+        client.publish(
+            "B2F/return", payload=json.dumps({"locatie": LOCATIE, "GUID": GUID}))
+        # actually doing something with the message...
 
-    if "status" in dict.keys():
-        if(LOCATIE == "onderweg naar kleedkamer" and dict["status"] == "gearriveerd"):
-            LOCATIE = "kleedkamer"
-            client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-        elif(LOCATIE == "onderweg naar sportscube" and dict["status"] == "gearriveerd"):
-            LOCATIE = "sportscube"
-            client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-        elif(LOCATIE == "onderweg naar onthaal" and dict["status"] == "gearriveerd"):
-            LOCATIE = "onthaal"
-            client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-        else:
-            print(f"ERROR PARSING 'ARRIVED', CURRENT LOCATION: {LOCATIE}")
-
-
-    # actually doing something with the message...
 
 def connect(host, port, username=None, password=None):
     global ROBOT
@@ -91,6 +108,7 @@ def connect(host, port, username=None, password=None):
     # time.sleep(1)
 
     # return client
+
 
 if __name__ == '__main__':
     connect(HOST, PORT)
