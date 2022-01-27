512490e065109102b0bbe0e3112ffb7070b5ce9a
Sam Verdoodt <sam.verdoodt@student.howest.be>
2022-01-27 09:08:01 +0100

    movement test

--- backend/mqtt/Src/app.py
+++ backend/mqtt/Src/app.py
@@ -5,7 +5,8 @@ import paho.mqtt.publish as publish
 import json
 import time
 import ssl
-from Src import pytemi as temi
+import pytemi as temi
+
 
 LOCATIE = None
 GUID = None
@@ -15,6 +16,9 @@ PORT = 1883
 TEMI_SERIAL = "00121175512"
 ROBOT = ""
 # CA_CRT = "C:/ca.crt"
+
+MQTT_HOST = "40.113.96.140"
+MQTT_PORT = 1883
 
 
 def on_connect(client, userdata, flags, rc):
@@ -41,67 +45,86 @@ def on_message(client, userdata, msg):
     print("------------------------------------------------------")
 
     topic = msg.topic
-    
+
     data = msg.payload.decode("utf-8")
     dict = json.loads(data)
 
     if topic == "F2B/return":
         LOCATIE = dict["locatie"]
         GUID = dict["GUID"]
+        waypoint = "G2" + LOCATIE
         client.publish(
             "B2F/return", payload=json.dumps({"locatie": LOCATIE, "GUID": GUID}))
-        # actually doing something with the message...
+        resp = go_to_waypoint(client, waypoint)
+        if resp == 0:
+            client.publish(
+                "B2F/locatie", payload=json.dumps({"locatie": LOCATIE, "status": "arrived"}))
+
     else:
         if "connectionStatus" in dict.keys():
             LOCATIE = None
-            client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            client.publish(
+                "B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
             client.publish("B2F/connected", payload=json.dumps(dict))
 
-        # hoe controleer je of temi bezig? 
+        # hoe controleer je of temi bezig?
         if "locatie" in dict.keys():
             LOCATIE = dict["locatie"]
-            client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            client.publish(
+                "B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
+            time.sleep(2)
             if (LOCATIE == "onderweg naar kleedkamer"):
-                # ga naar kleedkamer
-                pass
+                resp = go_to_waypoint(client, "g2kleedkamer")
+                if resp == 0:
+                    client.publish(
+                        "B2F/locatie", payload=json.dumps({"locatie": "kleedkamer", "status": "arrived"}))
 
             elif (LOCATIE == "onderweg naar sportscube"):
-                # ga naar sportscube
-                pass
+                resp = go_to_waypoint(client, "g2sportscube")
+                if resp == 0:
+                    client.publish(
+                        "B2F/locatie", payload=json.dumps({"locatie": "sportscube", "status": "arrived"}))
 
             elif (LOCATIE == "onderweg naar onthaal"):
-                # ga naar onthaal
-                pass
+                resp = go_to_waypoint(client, "g2onthaal")
+                if resp == 0:
+                    client.publish(
+                        "B2F/locatie", payload=json.dumps({"locatie": "onthaal", "status": "arrived"}))
+
             # for testing
 
-        if "status" in dict.keys():
-            if(LOCATIE == "onderweg naar kleedkamer" and dict["status"] == "gearriveerd"):
-                LOCATIE = "kleedkamer"
-                client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-            elif(LOCATIE == "onderweg naar sportscube" and dict["status"] == "gearriveerd"):
-                LOCATIE = "sportscube"
-                client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-            elif(LOCATIE == "onderweg naar onthaal" and dict["status"] == "gearriveerd"):
-                LOCATIE = "onthaal"
-                client.publish("B2F/locatie", payload=json.dumps({"locatie": LOCATIE}))
-            else:
-                print(f"ERROR PARSING 'ARRIVED', CURRENT LOCATION: {LOCATIE}")
 
+def go_to_waypoint(client, waypoint):
+    print("In goto")
+    global ROBOT
+    ROBOT.goto(waypoint)
+    time.sleep(2)
+    while ROBOT.goto_status != ROBOT.GOTO_COMPLETE:
+        if ROBOT.goto_status == ROBOT.GOTO_ABORT:  # Temi could not arrive
+            ROBOT.stop()
+            client.publish(
+                "B2F/admin", payload=json.dumps({"message": "Movement abborted", "locatie": waypoint}))
+            sleep(1)
+            return -1
+
+    # Temi arrived
+    return 0
 
 
 def connect(host, port, username=None, password=None):
     global ROBOT
-
     # create a new MQTT client instance
     client = mqtt.Client()
     # client.tls_set(tls_version=2)
     # client.tls_set(ca_certs=CA_CRT, tls_version=ssl.PROTOCOL_TLSv1_2)
     # attach general callbacks
+
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_message = on_message
 
-    ROBOT = temi.Robot(client, TEMI_SERIAL)
+    mqtt_client = temi.connect(MQTT_HOST, MQTT_PORT)
+    ROBOT = temi.Robot(mqtt_client, TEMI_SERIAL)
 
     # connect to MQTT broker
     client.connect(host=host, port=port, keepalive=60)
